e118283a20a2d001ba17e3cc329c16482de1f322
SuCicada <pengyifu@gmail.com>
2024-09-19 14:06:07 +0900

    some test

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,22 @@
 FROM node:20-alpine AS build
 WORKDIR /app
+#COPY . .
+#RUN npx pnpm i && npx pnpm build
+
+# 安装依赖
+# Install dependencies based on the preferred package manager
+COPY package.json yarn.lock* package-lock.json* pnpm-lock.yaml* ./
+RUN \
+  if [ -f yarn.lock ]; then yarn --frozen-lockfile; \
+  elif [ -f package-lock.json ]; then npm ci; \
+  elif [ -f pnpm-lock.yaml ]; then corepack enable pnpm && pnpm i --frozen-lockfile; \
+  else echo "Lockfile not found." && exit 1; \
+fi
+
+# 复制剩余的项目文件
 COPY . .
-RUN npx pnpm i && npx pnpm build
+#COPY .env.production .env.production
+RUN pnpm build
 
 FROM node:20-alpine
 WORKDIR /app
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,23 +1,50 @@
 import express from "express";
 
 const router = express.Router();
+const testRouter = express.Router();
 import {WebhookEventDefinition} from "@octokit/webhooks/dist-types/types";
 
 import {Webhooks} from "@octokit/webhooks";
 import * as fs from "node:fs";
 import {dealGithubPayload} from "./github";
 
+const APPRISE = true
+
+router.post('/github-test', async function (req, res, next) {
+  const data = req.body;
+  req.headers = data.header
+  req.body = data.body
+  req.url = '/github'; // 修改请求的 URL
+  next();
+})
 
 router.post('/github', async function (req, res, next) {
-  let payload = {
-    header: req.headers,
-    body: req.body
-  }
-  // console.log(payload);
-  // let filename = `data/data${new Date().getTime()}.json`;
-  // fs.writeFileSync(filename, JSON.stringify(payload, null, 2));
+  console.log("github webhook", req.headers['x-github-event']);
+  if (APPRISE) {
+    let payload = {
+      header: req.headers,
+      body: req.body
+    }
+    // console.log(payload);
+    // let filename = `data/data${new Date().getTime()}.json`;
+    // fs.writeFileSync(filename, JSON.stringify(payload, null, 2));
 
-  await dealGithubPayload(payload)
+    await dealGithubPayload(payload)
+  }else{
+    const headers = { ...req.headers };
+    delete headers['host'];
+    delete headers['content-length'];
+    delete headers['connection'];
+
+    let response = await fetch("", {
+      method: 'POST',
+      headers: headers as any,
+      body: JSON.stringify(req.body),
+    })
+    const res = await response.text();
+    // return res;
+    console.log("res: ",response.status, res);
+  }
   res.send({status: "ok"});
   // res.render('index', { title: 'Express' });
 // let a = {} as WebhookEventDefinition<"push">;
